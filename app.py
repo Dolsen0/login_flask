9452a874e25eba07839370d8448625e8a508ee0e
dolsen0 <derek.olsen@gmail.com>
2023-05-15 18:03:10 -0400

    added signup form

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ def index():
 def login():
     return render_template('login.html')
 
+@app.route('/register')
+def register():
+    return render_template('signup.html')
+
 @app.route('/about')
 def about():
     return render_template('about.html')
